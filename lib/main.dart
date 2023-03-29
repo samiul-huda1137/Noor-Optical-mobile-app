8757417bed4378ebb2b77d4376d15d527a5d161e
Sheikh Samiul Huda <samiul.huda1137@gmail.com>
2023-03-29 12:08:23 +0600

    page update

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,20 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:noor_optical/pages/powergroup.dart';
 import 'package:get/get.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp().then((value) => Get.put(AuthController()));
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Noor Optical',
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
